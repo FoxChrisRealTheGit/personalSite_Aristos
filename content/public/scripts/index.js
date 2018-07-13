acde0ce05b672ed48630bbbb1dc50711e543e20e
Christopher Fox <cfox1290@gmail.com>
2018-07-13 10:30:20 -0600

    responsive behavior added

--- content/public/scripts/index.js
+++ content/public/scripts/index.js
@@ -1,3 +1,6 @@
-import $ from "jquery";
 import AristosTextEditor from "./modules/AristosTextEditor";
 import confirmDelete from "./modules/confirmDelete";
+import MobileMenu from "./modules/MobileMenu";
+import $ from "jquery";
+
+const mobileMenu = new MobileMenu();
