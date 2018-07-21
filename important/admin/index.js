71aab7f6b5a6c0d5f469532c405167eac0fb7f49
Christopher Fox <cfox1290@gmail.com>
2018-07-21 11:00:10 -0600

    typos

--- important/admin/index.js
+++ important/admin/index.js
@@ -15,7 +15,7 @@ module.exports = app => {
   app.use("/admin/user-controls", adminUserControls);
   app.use("/admin", adminHome);
   /* */
-  console.log(process.NODE_ENV)
+  console.log(process.env.NODE_ENV)
   /* */
   require("../../expansion")(app);
   require("../../content/theme")(app);
