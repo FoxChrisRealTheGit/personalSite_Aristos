b2f67f35376bcf051a9336d04087e16e89e9fbe8
Christopher Fox <cfox1290@gmail.com>
2018-07-21 11:11:51 -0600

    testing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let app;
-if (process.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app = require("./dist/important/app.js.gz");
 } else {
   app = require("./important/app");
